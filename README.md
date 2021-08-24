6ed655d672bb14b31c82ce4261f31a5fa6a4de1f
jinhan814 <jinhan814@naver.com>
2021-08-25 03:34:45 +0900

    .

---
+++ README.md
@@ -0,0 +1,9 @@
+# BOJ
+
+## About Me
+
+BOJ handle : jinhan814 ([https://www.acmicpc.net/user/jinhan814](https://www.acmicpc.net/user/jinhan814))
+
+## Info
+
+백준 문제 풀이 코드를 올리는 공간입니다.
---
+++ cpp/1000.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+int main() {
+    fastio;
+    int a, b; cin >> a >> b;
+    cout << a + b << '\n';
+}
