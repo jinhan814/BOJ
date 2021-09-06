f6ea826c2cd6c9f5844e67d87694eeb248083fbb
jinhan814 <jinhan814@naver.com>
2021-09-07 03:14:10 +0900

    2021-09-07

---
+++ cpp/22981.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+#define int int64_t
+
+int32_t main() {
+	fastio;
+	int n, k; cin >> n >> k;
+	vector<int> v(n);
+	for (auto& i : v) cin >> i;
+	sort(v.begin(), v.end());
+
+	auto Sol = [&](int idx) -> int {
+		int t1 = v[0] * idx;
+		int t2 = v[idx] * (n - idx);
+		return (k + t1 + t2 - 1) / (t1 + t2);
+	};
+
+    int ans = int(1e18) + 7;
+    for (int i = 1; i < n; i++) ans = min(ans, Sol(i));
+    cout << ans << '\n';
+}
