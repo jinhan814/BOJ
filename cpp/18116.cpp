82c3b83f0824e8198ee08a46820021fb9ac74d23
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2021-09-08 16:00:34 +0900

    Update 18116.cpp

--- cpp/18116.cpp
+++ cpp/18116.cpp
@@ -17,17 +17,17 @@ struct UnionFind {
 
 int main() {
     struct stat st; fstat(0, &st);
-	char *p = (char*)mmap(0, st.st_size, PROT_READ, MAP_SHARED, 0, 0);
-	auto ReadInt = [&]() {
-		int ret = 0;
-		for (char c = *p++; c & 16; ret = 10 * ret + (c & 15), c = *p++);
-		return ret;
-	};
+    char *p = (char*)mmap(0, st.st_size, PROT_READ, MAP_SHARED, 0, 0);
+    auto ReadInt = [&]() {
+    	int ret = 0;
+    	for (char c = *p++; c & 16; ret = 10 * ret + (c & 15), c = *p++);
+    	return ret;
+    };
     
     UnionFind<1'000'001> UF;
-	for (int q = ReadInt(); q--;) {
-		char c = *p++; p++;
-		if (c == 'I') UF.Union(ReadInt(), ReadInt());
-		else cout << -UF.par[UF.Find(ReadInt())] << '\n';
-	}
+    for (int q = ReadInt(); q--;) {
+    	char c = *p++; p++;
+    	if (c == 'I') UF.Union(ReadInt(), ReadInt());
+    	else cout << -UF.par[UF.Find(ReadInt())] << '\n';
+    }
 }
